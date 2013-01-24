5c064cb363c9c59bff4ea37282b6b1985a72a471
Adam Bozanich <adam.boz@gmail.com>
2013-01-24 14:01:02 -0800

    rails new chef-deploy-webapp-rails-test --old-style-hash -d postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChefDeployWebappRailsTest::Application.config.secret_token = '9a344e3f4be9718677fe95dd160546b38e01faf93411354f337a8fbe96bf124bfc3ce5acd9768d6543cb8eb04c9952c20754fd1663b7f8b1b89e9e70b6080504'
